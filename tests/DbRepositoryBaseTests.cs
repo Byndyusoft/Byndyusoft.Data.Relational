b5d264f90209ce92cc1b44010fe0f098e0742522
victor.govorov <victorgovorov@mail.ru>
2020-11-10 14:34:52 +0500

    added DbRepositoryBaseTests

---
+++ tests/DbRepositoryBaseTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Xunit;
+
+namespace Byndyusoft.Data.Relational
+{
+    public class DbRepositoryBaseTests
+    {
+        private readonly IDbSessionAccessor _sessionAccessor;
+        private readonly DbRepositoryImpl _repository;
+
+        private class DbRepositoryImpl : DbRepositoryBase
+        {
+            public DbRepositoryImpl(IDbSessionAccessor sessionAccessor) : base(sessionAccessor)
+            {
+            }
+
+            public new IDbSession Session => base.Session;
+        }
+
+
+        public DbRepositoryBaseTests()
+        {
+            _sessionAccessor = Mock.Of<IDbSessionAccessor>();
+            _repository = new DbRepositoryImpl(_sessionAccessor);
+        }
+
+        [Fact]
+        public void Session_CallsAccessor()
+        {
+            // Arrange
+            var session = Mock.Of<IDbSession>();
+            Mock.Get(_sessionAccessor).SetupGet(x => x.DbSession).Returns(session);
+
+            // Act
+            var result = _repository.Session;
+
+            // Assert
+            Assert.Same(session, result);
+        }
+    }
+}
