05d6bbf758c2e9c9c755f8c97e0785b9d516ed9f
victor.govorov <victorgovorov@mail.ru>
2020-11-16 15:11:29 +0500

    added net5.0 support in tests

--- tests/DbSessionQueryObjectExtensionsTests.cs
+++ tests/DbSessionQueryObjectExtensionsTests.cs
@@ -276,7 +276,7 @@ namespace Byndyusoft.Data.Relational
             Assert.Equal("test2", row2.Name);
         }
 
-#if NETCOREAPP3_1
+#if NETCOREAPP3_1 || NET5_0
 
         [Fact]
         public async Task Query_NullQueryObject_ThrowsException()
