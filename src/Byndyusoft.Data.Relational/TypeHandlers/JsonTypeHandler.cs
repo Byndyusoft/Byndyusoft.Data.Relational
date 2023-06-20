95f48c6f30d9a8e13191c44f460ece26ca571b13
Alex Grigorichev <grigorichev@byndyusoft.com>
2023-06-20 15:57:19 +0500

    Вынес JsonSerializerOptions в класс JsonTypeHandler, сделал гетер этого свойства приватным

--- src/Byndyusoft.Data.Relational/TypeHandlers/JsonTypeHandler.cs
+++ src/Byndyusoft.Data.Relational/TypeHandlers/JsonTypeHandler.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Data;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Dapper;
 
 namespace Byndyusoft.Data.Relational.TypeHandlers
 {
     public class JsonTypeHandler<T> : SqlMapper.TypeHandler<T?> where T : class
     {
+        /// <summary>
+        ///     Reassign if you want to use another JsonSerializerOptions by default
+        /// </summary>
+        public static JsonSerializerOptions DefaultSerializerOptions { private get; set; } = new()
+        {
+            Converters = { new JsonStringEnumConverter() },
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+        
         private readonly JsonSerializerOptions _jsonSerializerOptions;
 
         public JsonTypeHandler()
         {
-            _jsonSerializerOptions = DefaultJsonTypeHandlerOptions.SerializerOptions;
+            _jsonSerializerOptions = DefaultSerializerOptions;
         }
 
         public JsonTypeHandler(JsonSerializerOptions jsonSerializerOptions)
