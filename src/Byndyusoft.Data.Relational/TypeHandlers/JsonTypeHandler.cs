0a35a730e913f45d8cff528813faded14e7275b0
Alex Grigorichev <grigorichev@byndyusoft.com>
2023-06-21 09:29:55 +0500

    Вынес JsonTypeHandler в отдельный класс.

--- src/Byndyusoft.Data.Relational/TypeHandlers/JsonTypeHandler.cs
+++ src/Byndyusoft.Data.Relational/TypeHandlers/JsonTypeHandler.cs
@@ -6,22 +6,25 @@ using Dapper;
 
 namespace Byndyusoft.Data.Relational.TypeHandlers
 {
-    public class JsonTypeHandler<T> : SqlMapper.TypeHandler<T?> where T : class
+    public static class JsonTypeHandler
     {
         /// <summary>
         ///     Reassign if you want to use another JsonSerializerOptions by default
         /// </summary>
-        public static JsonSerializerOptions DefaultSerializerOptions { private get; set; } = new()
+        public static JsonSerializerOptions DefaultSerializerOptions { get; set; } = new()
         {
             Converters = { new JsonStringEnumConverter() },
             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
         };
-        
+    }
+    
+    public class JsonTypeHandler<T> : SqlMapper.TypeHandler<T?> where T : class
+    {
         private readonly JsonSerializerOptions _jsonSerializerOptions;
 
         public JsonTypeHandler()
         {
-            _jsonSerializerOptions = DefaultSerializerOptions;
+            _jsonSerializerOptions = JsonTypeHandler.DefaultSerializerOptions;
         }
 
         public JsonTypeHandler(JsonSerializerOptions jsonSerializerOptions)
